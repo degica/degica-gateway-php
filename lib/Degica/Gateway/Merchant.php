b9e29bf89842d0e1ffd536e7f120b74cf7d19c5c
Gregor MacDougall <gmacdougall@degica.com>
2013-09-04 13:19:34 -0700

    Privatize merchant fields.
    Change-Id: Ifaf5957719e7a12b2d6f043e39b945f224cc1302

--- lib/Degica/Gateway/Merchant.php
+++ lib/Degica/Gateway/Merchant.php
@@ -3,6 +3,27 @@
 namespace Degica\Gateway;
 
 class Merchant {
-    public $merchant_slug;
-    public $api_key;
+
+    private $merchant_slug;
+    private $api_key;
+
+    public function getMerchantSlug()
+    {
+        return $this->merchant_slug;
+    }
+
+    public function setMerchantSlug($merchant_slug)
+    {
+        $this->merchant_slug = $merchant_slug;
+    }
+
+    public function getApiKey()
+    {
+        return $this->api_key;
+    }
+
+    public function setApiKey($api_key)
+    {
+        $this->api_key = $api_key;
+    }
 }

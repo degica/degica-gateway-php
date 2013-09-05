c89a869485155553adf43bf1b09ccaaee20ee675
Gregor MacDougall <gmacdougall@degica.com>
2013-09-05 16:28:36 -0700

    Add a sample.
    Change-Id: I7b4dd165e412d586f0cdf19d3572a0f2875a1515

---
+++ sample/index.php
@@ -0,0 +1,38 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Degica\Gateway\Merchant;
+use Degica\Gateway\PaymentMethod;
+use Degica\Gateway\Transaction;
+
+use Degica\Gateway\Request\CreateTransaction;
+
+$merchant = new Merchant();
+$merchant->setMerchantSlug('degica-mart');
+$merchant->setApiKey('42f89dc68c192645c2a752e907fcf648c2ff6782f7c18dffa0b6decd3ec7b030');
+
+$transaction = new Transaction($merchant);
+$transaction->setPaymentMethod(PaymentMethod::CREDIT_CARD);
+$transaction->setAmount('12.40');
+$transaction->setCancelUrl('http://www.bing.com/');
+$transaction->setCurrency('JPY');
+$transaction->setExternalOrderNum(randomString());
+$transaction->setReturnUrl('http://www.google.com/');
+$transaction->setTax('1.24');
+
+$create_transaction = new CreateTransaction('ja'); // 'ja' = Japanese, 'en' = English
+$create_transaction->sandboxMode();
+$url = $create_transaction->getSignedUrl($transaction);
+
+echo $url;
+
+function randomString($length = 10)
+{
+    $characters = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $randomString;
+}
